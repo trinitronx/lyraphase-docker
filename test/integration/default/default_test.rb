3706e4443ff30e765c421d3d6536d53fb4e74efd
James Cuzella <james.cuzella@lyraphase.com>
2020-07-19 05:21:03 -0600

    Use only_if to skip InSpec Control rather than omitting

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -30,12 +30,20 @@ test_driver = input('test_driver')
 version = input('docker_version')
 version_regexp = Regexp.new(version.to_s, Regexp::IGNORECASE | Regexp::MULTILINE)
 
-describe command('/usr/bin/docker --version') do
+describe command('docker --version') do
   its(:exit_status) { should eq 0 }
   its(:stdout) { should match(version_regexp) } if !version.nil? && !version.empty?
 end
 
-if test_driver != 'dokken'
+control 'docker-daemon-active' do
+  impact 'critical'
+  title 'Check if Docker service is running'
+  desc 'Check docker.service is running & enabled. Check docker.sock exists and has correct permissions'
+
+  only_if('Integration tests are NOT running in Dokken') do
+    test_driver != 'dokken'
+  end
+
   describe file('/var/run/docker.sock') do
     it { should exist }
     its('type') { should eq :socket }
@@ -48,10 +56,5 @@ if test_driver != 'dokken'
     it { should be_enabled }
     it { should be_installed }
     it { should be_running }
-  end
-
-  describe command('docker') do
-    it { should exist }
-    its('exit_status') { should eq 0 }
   end
 end
