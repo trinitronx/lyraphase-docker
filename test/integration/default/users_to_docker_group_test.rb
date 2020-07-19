84ca9606bb229bdbdb2a00e67bdb184219ba6efa
James Cuzella <james.cuzella@lyraphase.com>
2020-07-18 23:33:28 -0600

    Add InSpec tests for vagrant in docker group

---
+++ test/integration/default/users_to_docker_group_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+# Ensure that a user is added to the Docker group
+
+describe group('docker') do
+  it { should exist }
+end
+
+describe user('vagrant') do
+  it { should exist }
+  its('groups') { should include 'docker'}
+end
