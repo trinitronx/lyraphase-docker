78a40cb56f436264b1dbd146f51cc36dceafcb02
James Cuzella <james.cuzella@lyraphase.com>
2020-07-18 22:43:13 -0600

    InSpec checks for /var/run/docker.sock

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
 # InSpec test for recipe lyraphase-docker::default
 
 # The InSpec reference, with examples and extensive documentation, can be
 # found at https://www.inspec.io/docs/reference/resources/
 
 # This is an example test, replace it with your own test.
-describe file() do
+describe file('/var/run/docker.sock') do
   it { should exist }
   its('type') { should eq :socket }
   its('owner') { should eq 'root' }
   its('group') { should eq 'docker' }
+  its('mode') { should cmp '0660' }
 end
 
 describe systemd_service('docker') do
