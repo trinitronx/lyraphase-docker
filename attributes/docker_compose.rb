46a279edfb535b811fed995e9dbbe4da48075ff4
James Cuzella <james.cuzella@lyraphase.com>
2020-07-19 00:32:58 -0600

    lyraphase::docker_compose: Add recipe to install docker-compose

---
+++ attributes/docker_compose.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+default['lyraphase-docker']['compose']['install_binary'] = false
+default['lyraphase-docker']['compose']['version'] = '' # Default is to install latest available package
---
+++ recipes/docker_compose.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+#
+# Cookbook Name:: lyraphase-docker
+# Recipe:: docker_compose
+
+compose_config = node['lyraphase-docker']['compose']
+
+if compose_config['install_binary']
+  local_binary = File.join('/usr/local/bin/docker-compose')
+  github_downloads = 'https://github.com/docker/compose/releases/download'
+  remote = compose_config['binary_location'] || "#{github_downloads}/#{compose_config['version']}/docker-compose-Linux-x86_64"
+
+  remote_file local_binary do
+    source remote
+    owner 'root'
+    group 'root'
+    mode '0755'
+  end
+else
+  package 'docker-compose' do
+    version compose_config['version'] if ! compose_config['version'].nil? && ! compose_config['version'].empty?
+    action :install
+  end
+end
